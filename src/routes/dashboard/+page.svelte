b862df001bf143c6ed79ec391845a5545c3a3c35
Aditya Rayapudi <arayapudi2006@gmail.com>
2025-04-26 23:33:34 -0400

    Added Basic Chart

---
+++ src/routes/dashboard/+page.svelte
@@ -0,0 +1,36 @@
+<script>
+    let ctx;
+    import {Chart} from 'chart.js/auto';
+	import { onMount } from 'svelte';
+
+    onMount (() => {
+        new Chart(ctx, {
+        type: 'bar',
+        data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            borderWidth: 1
+        }]
+        },
+        options: {
+        scales: {
+            y: {
+            beginAtZero: true
+            }
+        }
+        }
+    });
+    })
+
+
+</script>
+
+
+<div>
+    <canvas bind:this={ctx} id="myChart"></canvas>
+</div>
+
+<div>Hi</div>
+  
